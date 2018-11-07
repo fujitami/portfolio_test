9c83822bcec3f36b2bdcb2b1eadf340ccccc29b7
fujitami <midori.fujita.0423@gmail.com>
2018-11-08 08:48:40 +0900

    Update script.js
    - スクロールが二重に出ないよう、
    iziModalでモーダル生成の際一緒に生成されるclass="iziModal-wrap"の
    class要素名を除去するように実装
    ※モーダルのヘッダーの下だけがスクロールする形に。
    - モーダルの角を調整
    - トップページのメニューボタンをマウスオーバーすると色がつく仕掛けを実装

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,24 +1,56 @@
-$('#profile__button').click(function(e) {
+$('.menu__list-button').hover(function() {
+  $(this).animate({
+    'backgroundColor': '#66BB6A'
+  }, 200);
+}, function() {
+  $(this).animate({
+    'backgroundColor': ''
+  }, 0);
+});
+
+function toggleClassFunc() {
+  if ($(this).parent().is('.modal-window')) {
+    return 'iziModal-wrap';
+  } else {
+    return 'scrollOK';
+  }
+}
+
+$('#menu__list').on('click', '#profile__button', function(e) {
   e.preventDefault();
   $('#profile__detail').iziModal('open');
+  $('.iziModal-wrap').toggleClass(toggleClassFunc);
 });
-$('#profile__detail').iziModal({
+$('#profile__detail').iziModal().css({
+  borderRadius: '0'
 });
 
-$('#skill__button').click(function(e) {
+$('#menu__list').on('click', '#skill__button', function(e) {
   e.preventDefault();
-  $('#skill__detail').iziModal('open');
+  $('.skill__detail').iziModal('open');
+  $('.iziModal-wrap').toggleClass(toggleClassFunc);
 });
-$('#skill__detail').iziModal();
+$('.skill__detail').iziModal({
+  group: 'skillGroup',
+  loop: true
+}).css({
+  borderRadius: '0'
+});
 
-$('#product__button').click(function(e) {
+$('#menu__list').on('click', '#product__button', function(e) {
   e.preventDefault();
   $('#product__detail').iziModal('open');
+  $('.iziModal-wrap').toggleClass(toggleClassFunc);
 });
-$('#product__detail').iziModal();
+$('#product__detail').iziModal().css({
+  borderRadius: '0'
+});
 
-$('#other__button').click(function(e) {
+$('#menu__list').on('click', '#other__button', function(e) {
   e.preventDefault();
   $('#other__detail').iziModal('open');
+  $('.iziModal-wrap').toggleClass(toggleClassFunc);
 });
-$('#other__detail').iziModal();
+$('#other__detail').iziModal().css({
+  borderRadius: '0'
+});
